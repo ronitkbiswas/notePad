23b990a43c43c49297129454c69f7483d5896fd5
Ronit <ronitkbiswas@gmail.com>
2025-12-17 15:07:39 +0530

    css

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const input = document.getElementById("noteInput");
 const addBtn = document.getElementById("addBtn");
 const showNote = document.getElementById("showNote");
 
+// show if saved previously
 let notes = localStorage.getItem("notes");
 notes ? (notes = JSON.parse(notes)) : (notes = []);
-
 showNote.innerHTML = "";
-for (let i = 0; i < notes.length; i++) {
+for (let i = notes.length - 1; i >= 0; i--) {
   const li = document.createElement("li");
   li.innerHTML = `${notes[i]} <button data-index="${i}">x</button>`;
   showNote.appendChild(li);
@@ -20,19 +20,20 @@ addBtn.addEventListener("click", function () {
   localStorage.setItem("notes", JSON.stringify(notes));
   input.value = "";
   showNote.innerHTML = "";
-  for (let i = 0; i < notes.length; i++) {
+  for (let i = notes.length - 1; i >= 0; i--) {
     const li = document.createElement("li");
     li.innerHTML = `${notes[i]} <button data-index="${i}">x</button>`;
     showNote.appendChild(li);
   }
 });
+// delete
 showNote.addEventListener("click", (e) => {
   if (e.target.tagName !== "BUTTON") return;
   const index = e.target.dataset.index;
   notes.splice(index, 1);
   localStorage.setItem("notes", JSON.stringify(notes));
   showNote.innerHTML = "";
-  for (let i = 0; i < notes.length; i++) {
+  for (let i = notes.length - 1; i >= 0; i--) {
     const li = document.createElement("li");
     li.innerHTML = `${notes[i]} <button data-index="${i}">x</button>`;
     showNote.appendChild(li);
